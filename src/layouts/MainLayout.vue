c362f5eb10d8f547b983a29c98e54a4a72b37bef
jinxkitt108 <clint.stupra@gmail.com>
2020-08-29 10:36:48 +0800

    Added discussions and portfolio pages

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -22,14 +22,58 @@
           to="/admin/dashboard"
           label="Admin"
           flat
+          exact
         />
         <q-btn
           v-if="Object.keys(currentUser).length"
-          @click="logoutUser"
+          @click="logout"
           color="warning"
           label="Log Out"
           flat
         />
+        <q-btn
+          v-else
+          @click="openDialog"
+          color="warning"
+          label="Log in"
+          flat
+        />
+        <!-- Log in dialog -->
+        <q-dialog v-model="login_dialog">
+            <q-card class="q-pa-md" style="width: 400px">
+                <div class="text-h5 text-center">Log In</div>
+              <q-card-section>
+                <q-form class="q-gutter-md">
+                  <q-input label="Email" type="email" rounded outlined dense />
+                  <q-input label="Password" :type="isPwd ? 'password' : 'text'" rounded outlined dense>
+                    <template v-slot:append>
+                      <q-icon @click="isPwd = !isPwd" :name="isPwd ? 'visibility' : 'visibility_off'" />
+                    </template>
+                  </q-input>
+                  <div class="q-px-sm">
+                    <q-btn label="Log in" class="full-width" color="indigo" rounded />
+                  </div>
+                </q-form>
+              </q-card-section>
+              <q-card-section>
+                <hr><span>OR</span><hr>
+              </q-card-section>
+              <q-card-section class="q-gutter-md">
+                <q-btn class="full-width" no-caps color="blue-8" >
+                  <q-avatar size="42px" class="q-mr-sm">
+                    <img src="/social_media_icons/svg/facebook.svg">
+                  </q-avatar>
+                  Sign In with Facebook
+                </q-btn>
+                <q-btn class="full-width" no-caps color="red-5">
+                  <q-avatar size="42px" class="q-mr-sm">
+                    <img src="/social_media_icons/svg/google-plus.svg">
+                  </q-avatar>
+                  Sign In with Google
+                </q-btn>
+              </q-card-section>
+            </q-card>
+        </q-dialog>
         <q-btn
           @click="drawer = !drawer"
           class="mobile-only"
@@ -115,6 +159,8 @@ export default {
   data() {
     return {
       drawer: null,
+      login_dialog: false,
+      isPwd: true,
       links: [
         { name: "Home", path: "/" },
         { name: "Portfolio", path: "/portfolio" },
@@ -129,7 +175,13 @@ export default {
   },
 
   methods: {
-    ...mapActions("auth", ["logoutUser"])
+    ...mapActions("auth", ["logoutUser"]),
+    openDialog() {
+      this.login_dialog = true
+    },
+    logout() {
+      this.logoutUser()
+    }
   }
 };
 </script>
